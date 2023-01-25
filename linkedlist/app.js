a559691c71be55e2837252e2838d198c80ed1b18
benrobo <alumonabenaiah71@gmail.com>
2023-01-24 18:43:01 -0800

    added linkedlist node

---
+++ linkedlist/app.js
@@ -0,0 +1,42 @@
+// linkedlist is basically a linear data structure where each element links to the next element on the list.
+
+const { log } = console;
+
+
+class LinkedList {
+
+}
+
+class Node {
+    next = null;
+    prev = null;
+    constructor(data) {
+        this.data = data;
+        // this.next = null;
+    }
+
+    countNodes(head) {
+        let count = 1;
+        let curr = head;
+        while (curr.next !== null) {
+            curr = curr.next;
+            count++;
+        }
+        log(count)
+    }
+}
+
+const head = new Node(2)
+const n2 = new Node(4)
+const n3 = new Node(6)
+const n4 = new Node(8)
+const n5 = new Node(10)
+const n6 = new Node(12)
+
+head.next = n2;
+n2.next = n3;
+n3.next = n4;
+// n4.next = n5;
+// n5.next = n6;
+
+head.countNodes(head)
